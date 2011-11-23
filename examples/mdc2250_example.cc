1c3cb7d745ded2aeff389e50bc370f8258998322
William Woodall <wjwwood@gmail.com>
2011-11-23 12:40:36 -0600

    Updating examples.

--- examples/mdc2250_example.cc
+++ examples/mdc2250_example.cc
@@ -2,15 +2,35 @@
 
 int main(void) {
   mdc2250::MDC2250 my_mdc2250;
+  mdc2250::MDC2250 my_mdc2250_1;
+
   
-  my_mdc2250.connect("/dev/tty.usbmodemfd1341");
-  my_mdc2250.setCommand(mdc2250::commanditem::_GO, 255, 1);
-  my_mdc2250.setCommand(mdc2250::commanditem::_GO, 255, 2);
-  // mdc2250::sleep_ms(1000);
-  sleep(1);
+  my_mdc2250.connect("/dev/ttyACM0");
+  my_mdc2250_1.connect("/dev/ttyACM1");
+
+  for(size_t i = 0; i < 10; ++i) {
+    my_mdc2250.setCommand(mdc2250::commanditem::_GO, -255, 1);
+    my_mdc2250.setCommand(mdc2250::commanditem::_GO, 255, 2);
+    my_mdc2250_1.setCommand(mdc2250::commanditem::_GO, -255, 1);
+    my_mdc2250_1.setCommand(mdc2250::commanditem::_GO, 255, 2);
+    usleep(1000*1000);
+  }
+
+  for(size_t i = 0; i < 10; ++i) {
+    my_mdc2250.setCommand(mdc2250::commanditem::_GO, 255, 1);
+    my_mdc2250.setCommand(mdc2250::commanditem::_GO, -255, 2);
+    my_mdc2250_1.setCommand(mdc2250::commanditem::_GO, 255, 1);
+    my_mdc2250_1.setCommand(mdc2250::commanditem::_GO, -255, 2);
+    usleep(1000*1000);
+  }
+
   my_mdc2250.setCommand(mdc2250::commanditem::_GO, 0, 1);
   my_mdc2250.setCommand(mdc2250::commanditem::_GO, 0, 2);
+  my_mdc2250_1.setCommand(mdc2250::commanditem::_GO, 0, 1);
+  my_mdc2250_1.setCommand(mdc2250::commanditem::_GO, 0, 2);
+
   my_mdc2250.disconnect();
-  
+  my_mdc2250_1.disconnect();
+
   return 0;
 }
