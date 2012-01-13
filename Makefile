253ece7b6d2c40a4bbdd027e1bb14a364a11dd82
William Woodall <wjwwood@gmail.com>
2012-01-13 15:31:29 -0600

    New build system

--- Makefile
+++ Makefile
@@ -1,53 +1,5 @@
-UNAME := $(shell uname)
-
-all: mdc2250
-
-install:
-	cd build && make install
-
-uninstall:
-	cd build && make uninstall
-
-mdc2250:
-	@mkdir -p build
-	@mkdir -p bin
-	cd build && cmake $(CMAKE_FLAGS) ..
-ifneq ($(MAKE),)
-	cd build && $(MAKE)
+ifdef ROS_ROOT
+include $(shell rospack find mk)/cmake.mk
 else
-	cd build && make
+include mdc2250.makefile
 endif
-
-example:
-	@mkdir -p build
-	@mkdir -p bin
-	cd build && cmake $(CMAKE_FLAGS) -DMDC2250_BUILD_EXAMPLES=1 ..
-ifneq ($(MAKE),)
-	cd build && $(MAKE)
-else
-	cd build && make
-endif
-	cd bin && ./mdc2250_example
-
-clean:
-	-cd build && make clean
-	rm -rf build bin lib doc/html
-
-.PHONY: doc
-doc:
-	@doxygen doc/Doxyfile
-ifeq ($(UNAME),Darwin)
-	@open doc/html/index.html
-endif
-
-.PHONY: test
-test:
-	@mkdir -p build
-	@mkdir -p bin
-	cd build && cmake $(CMAKE_FLAGS) -DMDC2250_BUILD_TESTS=1 -DMDC2250_BUILD_EXAMPLES=1 ..
-ifneq ($(MAKE),)
-	cd build && $(MAKE)
-else
-	cd build && make
-endif
-	cd bin && ./mdc2250_tests
---
+++ mdc2250_ros.cmake
@@ -0,0 +1,35 @@
+macro(build_mdc2250)
+cmake_minimum_required(VERSION 2.4.6)
+include($ENV{ROS_ROOT}/core/rosbuild/rosbuild.cmake)
+
+# Set the build type.  Options are:
+#  Coverage       : w/ debug symbols, w/o optimization, w/ code-coverage
+#  Debug          : w/ debug symbols, w/o optimization
+#  Release        : w/o debug symbols, w/ optimization
+#  RelWithDebInfo : w/ debug symbols, w/ optimization
+#  MinSizeRel     : w/o debug symbols, w/ optimization, stripped binaries
+set(ROS_BUILD_TYPE RelWithDebInfo)
+
+rosbuild_init()
+
+#set the default path for built executables to the "bin" directory
+set(EXECUTABLE_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/bin)
+#set the default path for built libraries to the "lib" directory
+set(LIBRARY_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/lib)
+
+include_directories(include)
+
+set(MDC2250_SRCS src/mdc2250.cc)
+
+# Build the mdc2250 library
+rosbuild_add_library(${PROJECT_NAME} ${MDC2250_SRCS})
+
+# Add boost dependencies
+rosbuild_add_boost_directories()
+rosbuild_link_boost(${PROJECT_NAME} system filesystem thread)
+
+# Build example
+rosbuild_add_executable(mdc2250_example examples/mdc2250_example.cc)
+target_link_libraries(mdc2250_example ${PROJECT_NAME})
+
+endmacro(build_mdc2250)
